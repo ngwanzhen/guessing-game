1252cdde0be0e519c0d9ab5128c2ab75f50ba01f
Wanzhen Ng <ngwanzhen@gmail.com>
2017-06-22 17:28:00 +0800

    inital upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+var h1 = document.querySelector('h1')
+var body = document.querySelector('body')
+
+// var userIn = parseInt(prompt('pls enter your guess'))
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
+var ans = getRandomIntInclusive(1, 5)
+console.log(ans)
+
+// var state = false
+// to get start game button, end game button, restart
+
+
+function higherOrLower (userIn, ans) {
+  if (userIn !== ans) {
+    if (userIn > ans) {h1.innerText = 'lower pls'
+      return setTimeout(getUser, 1000)}
+        else if (userIn < ans) {h1.innerText = 'higher pls'
+          return setTimeout(getUser, 1000)}
+    }
+    body.style.backgroundColor = 'green'
+}
+
+function getUser () {
+  var userIn = prompt('key in n')
+  if (userIn !== ans) { return higherOrLower(userIn, ans) }
+    else if (userIn == ans) { console.log('you won') }
+  // return higherOrLower(userIn, ans)
+}
+
+//function should get input,
+//check and change css
+//decide if should loop back to get input
+setTimeout(getUser, 1000)
