885fde4cd4aead84511ec5aa4f5884d552690770
Wanzhen Ng <ngwanzhen@gmail.com>
2017-06-23 10:21:49 +0800

    fixed restart

--- script.js
+++ script.js
@@ -3,44 +3,51 @@ var body = document.querySelector('body')
 
 // var userIn = parseInt(prompt('pls enter your guess'))
 
-
 // **Update: Button added to start game;
 // issues: 1) re-starting game does not generate new random number only page re-load does
-document.getElementById("startBtn").addEventListener("click", getUser)
+document.getElementById('startBtn').addEventListener('click', setTimeout(getUser, 1000))
+document.getElementById('startBtn').addEventListener('click', restart)
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+
+function getRandomIntInclusive (min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min // The maximum is inclusive and the minimum is inclusive
 }
-
-
 
 var ans = getRandomIntInclusive(1, 5)
 console.log(ans)
 
 function higherOrLower (userIn, ans) {
   if (userIn !== ans) {
-    if (userIn > ans) {h1.innerText = 'lower pls'
-      return setTimeout(getUser, 1000)}
-        else if (userIn < ans) {h1.innerText = 'higher pls'
-          return setTimeout(getUser, 1000)}
+    if (userIn > ans) {
+      h1.innerText = 'lower pls'
+      return setTimeout(getUser, 1000)
+    } else if (userIn < ans) {
+      h1.innerText = 'higher pls'
+      return setTimeout(getUser, 1000)
     }
-    body.style.backgroundColor = 'green'
+  }
+  body.style.backgroundColor = 'green'
+  h1.innerText = 'Game Won'
 }
 
 // **Update: Modified getUser to end game on 'cancel' and 'empty prompt'.
 function getUser () {
   var userIn = prompt('key in n')
-  if (userIn === "" || userIn === null) {return;}
-  else if (userIn !== ans) { return higherOrLower(userIn, ans) }
-    else if (userIn == ans) { console.log('you won') }
-    else {return;}
+  if (userIn === '' || userIn === null) { return } else if (userIn !== ans) { return higherOrLower(userIn, ans) } else if (userIn == ans) { console.log('you won') } else { return }
   // return higherOrLower(userIn, ans)
 }
 
+function restart () {
+  h1.innerText = 'please key in a number to start guessing'
+  body.style.backgroundColor = 'rgb(150, 75, 27)'
+  setTimeout(getUser, 1000)
+  ans = getRandomIntInclusive(1, 5)
+  console.log(ans)
+}
 
-//function should get input,
-//check and change css
-//decide if should loop back to get input
+// function should get input,
+// check and change css
+// decide if should loop back to get input
 // setTimeout(getUser, 1000) **Update:removed to prevent auto-start upon page load**
